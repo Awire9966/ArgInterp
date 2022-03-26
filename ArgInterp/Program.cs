e72cb8076dc26641f8735ad29906b74a669c7492
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2022-03-26 10:19:37 -0400

    Add files via upload

---
+++ ArgInterp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ArgInterp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading Args");
+            Console.WriteLine("Saving to:" +Environment.CurrentDirectory + @"\args.txt");
+            File.WriteAllLines(Environment.CurrentDirectory + @"\args.txt", args);
+
+        }
+    }
+}
